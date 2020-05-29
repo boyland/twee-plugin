cdf12196117cc69ecfd3caa4fe191050fa63c247
John Boyland <boyland@uwm.edu>
2020-05-29 17:28:00 -0500

    Added passage syntax

--- src/edu/uwm/twee/editors/SCTagScanner.java
+++ src/edu/uwm/twee/editors/SCTagScanner.java
@@ -10,15 +10,17 @@ public class SCTagScanner extends RuleBasedScanner {
 	public SCTagScanner(ColorManager manager) {
 		IToken string = new Token(new TextAttribute(manager.getColor(ITweeColorConstants.STRING)));
 		IToken template = new Token(new TextAttribute(manager.getColor(ITweeColorConstants.JS_TEMPLATE)));
-		IRule[] rules = new IRule[4];
+		IToken link = new Token(new TextAttribute(manager.getColor(ITweeColorConstants.SC_LINK)));
+		IRule[] rules = new IRule[5];
 
 		// Add rule for double quotes
 		rules[0] = new SingleLineRule("\"", "\"", string, '\\');
 		// Add a rule for single quotes
 		rules[1] = new SingleLineRule("'", "'", string, '\\');
 		rules[2] = new MultiLineRule("`", "`", template);
+		rules[3] = new MultiLineRule("[[","]]", link);
 		// Add generic whitespace rule.
-		rules[3] = new WhitespaceRule(new TweeWhitespaceDetector());
+		rules[4] = new WhitespaceRule(new TweeWhitespaceDetector());
 
 		setRules(rules);
 	}
